6eeea462871fbf2dfd9086eced9c198e7cbe4a3f
lane froehlich <elaine.froehlich@gmail.com>
2020-10-10 14:29:21 -0400

    Creating a class to count

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace OOPpmfsm
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hey big brother!");
+            var worker = new FactoryWorker();
+            worker.Count(4);
+            worker.Count(5);
+            //Console.WriteLine("Hey big brother!");
         }
     }
+
+    public class FactoryWorker
+    { 
+        public FactoryWorker ()
+        {
+            public void Count (int number)
+            {
+                // every time count is run add one to Total
+                if (Total == null)
+                    Total = 0;
+                Total = Total + number;
+                Console.WriteLine(Total.ToString());
+            }
+            public int Total( get: set: )
+
+             
+
+        }
+
+    }
+
 }
